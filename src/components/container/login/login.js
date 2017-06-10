3111ef038ba7a9c9279d4a0a52e220509acd6960
SAPIENT\isin17 <isingh17@sapient.com>
2017-06-10 19:04:44 +0530

    login added

--- src/components/container/login/login.js
+++ src/components/container/login/login.js
@@ -1,11 +1,30 @@
 import React from "react";
+import InputField from "../../presentational/inputField/inputField";
+import "./login.scss";
 
 class Login extends React.Component {
 	render() {
 		return(
-			<div>
-				<h1>Login</h1>
-			</div>
+			<div className="l-login col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6 col-lg-offset-4 col-lg-4">
+        <form action="/action_page.php">
+          <InputField
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            label="Email:"
+          />
+          <InputField
+            type="password"
+            placeholder="Enter password"
+            name="pwd"
+            label="Password:"
+          />
+          <div className="checkbox">
+            <label><input type="checkbox" name="remember" /> Remember me</label>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
 		);
 	}
 }
--- src/components/container/register/register.js
+++ src/components/container/register/register.js
@@ -7,22 +7,18 @@ class Register extends React.Component {
     return(
       <div className="l-register col-xs-offset-2 col-xs-8 col-sm-offset-3 col-sm-6 col-lg-offset-4 col-lg-4">
         <form action="/action_page.php">
-          <div className="col-xs-6">
-            <InputField
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              label="Email:"
-            />
-          </div>
-          <div className="col-xs-6">
-            <InputField
-              type="password"
-              placeholder="Enter password"
-              name="pwd"
-              label="Password:"
-            />
-          </div>
+          <InputField
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            label="Email:"
+          />
+          <InputField
+            type="password"
+            placeholder="Enter password"
+            name="pwd"
+            label="Password:"
+          />
           <div className="checkbox">
             <label><input type="checkbox" name="remember" /> Remember me</label>
           </div>
