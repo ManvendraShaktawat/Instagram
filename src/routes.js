cb31772416aabe613ba63ed159429aed48da4ce3
SAPIENT\isin17 <isingh17@sapient.com>
2017-06-11 21:10:46 +0530

    authentication added

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,27 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import {Route, Switch, Redirect} from "react-router-dom";
 import App from "./components/container/app/app";
 import HomePage from "./components/container/home/homePage";
 import Login from "./components/container/login/login";
 import Register from "./components/container/register/register";
 import NotFoundPage from "./components/presentational/notFound/notFoundPage";
 
+const authUser =  function() {
+	let user = sessionStorage.getItem("user");
+	return (
+		user ? (
+			<HomePage/>
+			) : (
+			<Redirect to="/login"/>
+		)
+	);
+};
+
 const routes = (
 	<App>
 		<Switch>
-			<Route name="login" exact path="/" component={Login} />
-			<Route name="home" exact path="/home" component={HomePage} />
+			<Route name="login" exact path="/login" component={Login} />
+			<Route name="home" exact render={authUser} path="/" />
 			<Route name="register" exact path="/register" component={Register} />
 			<Route component={NotFoundPage} />
 		</Switch>
